8086a2971c024cdb0dfe60aed0803f9cdfda0617
Pavwos <rspavanelli@gmail.com>
2022-08-19 22:15:53 -0300

    sim
    aham

---
+++ main.js
@@ -0,0 +1,27 @@
+function carregar(){
+
+    var texto = document.getElementById('msg')
+    var foto = document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+
+    if(hora >= 0 && hora < 6){
+        foto.src = 'tabem.png'
+        document.body.style.background = "#000"
+        texto.innerHTML = `Agora são ${hora} horas. Ta bem?`
+    }
+    else if(hora >= 6 && hora < 12){
+        foto.src = 'manha.png'
+        document.body.style.background = "#ffe668"
+        texto.innerHTML = `Agora são ${hora} horas. Bom dia`
+    } else if (hora >= 12 && hora < 18){
+        foto.src = 'tarde.png'
+        document.body.style.background = "#b9846f"
+        texto.innerHTML = `Agora são ${hora} horas. Boa tarde`
+    } else {
+        foto.src = 'noite.png'
+        document.body.style.background = "#0f2233"
+        texto.innerHTML = `Agora são ${hora} horas. Boa noite`
+    }
+
+};
