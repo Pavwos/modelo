dec67411e7ec65e9f2a8c96522a9f3f8c5c13ec5
Pavwos <rspavanelli@gmail.com>
2023-11-14 17:08:15 -0300

    layoutNovo
    hehe

--- main.js
+++ main.js
@@ -2,65 +2,46 @@ $(window).on("load", function(){
     $(".cssload-container").delay(1069).fadeOut(690);
 });
 
-function carregar(){
+function carregar() {
+    var texto = document.querySelectorAll('#msg, #bom');
+    var foto = document.getElementById('img');
+    var icone = document.querySelectorAll('.fa-solid');
+    var data, hora, minuto;
 
-    var texto = document.querySelectorAll('#topo, #msg, #more, #copy')
-    var foto = document.getElementById('img')
-    var icone = document.querySelectorAll('.fa-solid')
-    var data = new Date()
-    var hora = data.getHours()
-    var minuto = data.getMinutes()
-    
-    setInterval(function(){
-         data = new Date()
-         hora = data.getHours().toString().padStart(2, '0')
-         minuto = data.getMinutes().toString().padStart(2, '0')
-         
+    setInterval(function () {
+        data = new Date();
+        hora = data.getHours().toString().padStart(2, '0');
+        minuto = data.getMinutes().toString().padStart(2, '0');
 
-        if(hora >= 0 && hora < 6){
-            foto.src = 'tabem.png'
-            document.body.style.background = "#000"
-            icone[0].style.color = "#ffffff"
-            icone[1].style.color = "#ffffff"
-            icone[2].style.color = "#ffffff"
-            icone[3].style.color = "#ffffff"
-            texto[1].innerHTML = `Boa madrugada, agora são ${hora}:${minuto}`
-        }
-        else if(hora >= 6 && hora <= 12){
-            foto.src = 'manha.png'
-            for(a in icone){
-                if(icone[a].classList){
-                    icone[a].classList.add('manha')
-                }
-            }
-            for(i in texto){
-            if (texto[i].classList){
-                texto[i].classList.add('manha')
-            }   
-        }
-            texto[1].innerHTML = `Bom dia, agora são ${hora}:${minuto}`
-        } else if (hora >= 13 && hora < 18){
-            foto.src = 'tarde.png'
-            texto[0].style.color = "#000"
-            for(a in icone){
-                if(icone[a].classList)
-                icone[a].classList.add('tarde')
-            }
-            for(i in texto){
-                if (texto[i].classList){
-                    texto[i].classList.add('tarde')
-                }
-            }
-            texto[1].innerHTML = `Boa tarde, agora são ${hora}:${minuto}`
+        if (hora >= 0 && hora < 6) {
+            foto.src = 'tabem.png';
+            document.body.style.background = '#000'
+            icone.forEach(function (element) {
+                element.style.color = "#fff";
+            });
+            texto[1].innerHTML = `${hora}:${minuto}`;
+            texto[0].innerHTML = `Boa madruga`;
+        } else if (hora >= 6 && hora <= 12) {
+            document.body.style.background = '#fdc673'
+            icone.forEach(function (element) {
+                element.style.color = "#fff";
+            });
+            texto[1].innerHTML = `${hora}:${minuto}`;
+            texto[0].innerHTML = `Bom dia`;
+        } else if (hora >= 13 && hora < 18) {
+            document.body.style.background = '#de7d6a'
+            icone.forEach(function (element) {
+                element.style.color = "#fff";
+            });
+            texto[1].innerHTML = `${hora}:${minuto}`;
+            texto[0].innerHTML = `Boa tarde`;
         } else {
-            foto.src = 'noite.png'
-            icone[0].style.color = "#0a2472"
-            icone[1].style.color = "#0a2472"
-            icone[2].style.color = "#0a2472"
-            icone[3].style.color = "#0a2472"
-            texto[1].innerHTML = `Boa noite, agora são ${hora}:${minuto}`
+            document.body.style.background = '#020f20'
+            icone.forEach(function (element) {
+                element.style.color = "#fff";
+            });
+            texto[1].innerHTML = `${hora}:${minuto}`;
+            texto[0].innerHTML = `Boa noite`;
         }
-    }, 1000)
-    
-    
-};
+    }, 1000);
+}
