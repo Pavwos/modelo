d8bda62ced4409045e450af9073e2ee572e574bd
pavwos <rspavanelli@gmail.com>
2024-08-26 23:26:19 -0300

    mostrarHoras

--- main.js
+++ main.js
@@ -1,46 +1,53 @@
-$(window).on("load", function(){
-    $(".cssload-container").delay(1069).fadeOut(690);
+$(window).on("load", function () {
+    $('.content').hide();
+    $('.loader-content').delay(900).fadeOut(900, function () {
+        $('.content').fadeIn(500).show();
+    });
 });
 
 function carregar() {
-    var texto = document.querySelectorAll('#msg, #bom');
-    var foto = document.getElementById('img');
-    var icone = document.querySelectorAll('#icone');
-    var data, hora, minuto;
+    var texto = document.querySelectorAll('#time, #msg, #more, #at');
+    var fundo = document.querySelector('#bg-image');
+    var data, hora, minuto, segundo;
+    var box = document.querySelector('#seconds')
+    let showSeconds = false;
 
     setInterval(function () {
         data = new Date();
         hora = data.getHours().toString().padStart(2, '0');
         minuto = data.getMinutes().toString().padStart(2, '0');
+        segundo = data.getSeconds().toString().padStart(2, '0');
+
+        var mensagem, imagem;
 
         if (hora >= 0 && hora < 6) {
-            document.body.style.backgroundImage = 'url(img/madrugada.jpeg)'
-            icone.forEach(function (element) {
-                element.style.color = "##ffa600";
-            });
-            texto[1].innerHTML = `${hora}:${minuto}`;
-            texto[0].innerHTML = `Boa madruga`;
+            mensagem = "Boa madrugada :)";
+            imagem = "./img/madrugada.jpeg";
         } else if (hora >= 6 && hora <= 12) {
-            document.body.style.backgroundImage = 'url(img/madrugada.jpeg)'
-                icone.forEach(function (element) {
-                    element.style.color = "#fff";
-                });
-            texto[1].innerHTML = `${hora}:${minuto}`;
-            texto[0].innerHTML = `Bom dia`;
+            mensagem = "Bom dia :)";
+            imagem = "./img/manha.jpeg";
         } else if (hora >= 13 && hora < 18) {
-            document.body.style.backgroundImage = 'url(img/madrugada.jpeg)'
-            icone.forEach(function (element) {
-                element.style.color = "#fff";
-            });
-            texto[1].innerHTML = `${hora}:${minuto}`;
-            texto[0].innerHTML = `Boa tarde`;
+            mensagem = "Boa tarde :)";
+            imagem = "./img/tarde.jpeg";
         } else {
-            document.body.style.backgroundImage = 'url(img/madrugada.jpeg)'
-            icone.forEach(function (element) {
-                element.style.color = "#fff";
-            });
-            texto[1].innerHTML = `${hora}:${minuto}`;
-            texto[0].innerHTML = `Boa noite`;
+            mensagem = "Boa noite :)";
+            imagem = "./img/noite.jpeg";
         }
+
+        if (showSeconds) {
+            texto[0].innerHTML = `${hora}:${minuto}:${segundo}`;
+        } else {
+            texto[0].innerHTML = `${hora}:${minuto}`;
+        }
+
+        box.onchange = function () {
+            showSeconds = box.checked;
+        }
+
+        fundo.style.backgroundImage = `url('${imagem}')`;
+        fundo.style.filter = 'brightness(25%)';
+        texto[1].innerHTML = mensagem;
     }, 1000);
 }
+
+
